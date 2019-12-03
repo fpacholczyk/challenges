27d784311908d26771a34b236921e156db821271
Filip Pacholczyk <filip@pacholczyk.eu>
2019-12-03 19:39:43 +0100

    PCC01 Pythonize :)

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -2,29 +2,20 @@ from data import DICTIONARY, LETTER_SCORES
 
 def load_words():
     """Load dictionary into a list and return list"""
-    words = []
     with open(DICTIONARY, "r") as data:
-        for word in data:
-            words.append(word.strip())
-    return words
+        return [ word.strip() for word in data ]
 
 def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    return sum([LETTER_SCORES[l] if l in LETTER_SCORES else 0 for l in word.upper()])
+    return sum([LETTER_SCORES.get(l, 0) for l in word.upper()])
 
 def max_word_value(words = None):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
     if not words:
         words = load_words()
-    # max_score = (0, '')
-    # for word in words:
-    #     score = calc_word_value(word)
-    #     if score > max_score[0]:
-    #         max_score = (score, word)
-    # return max_score[1]
-    return max( ((calc_word_value(word), word) for word in words) )[1]
+    return max(words, key=calc_word_value)
 
 if __name__ == "__main__":
     pass # run unittests to validate
